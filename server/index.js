98fbf0a33fc29d00e21961aa325692480331bf7d
Nate Roundy <nroundy801@gmail.com>
2019-11-08 17:57:19 -0700

    Finished the addHouse FE post

--- server/index.js
+++ server/index.js
@@ -7,13 +7,13 @@ const {SERVER_PORT, CONNECTION_STRING} = process.env
 const app = express()
 app.use(express.json())
 
-app.post('/api/houses', ctrl.addHouse)
 app.get('/api/houses', ctrl.getAllHouses)
+app.post('/api/houses', ctrl.addHouse)
 
 massive(CONNECTION_STRING).then(databaseConnection => {
   app.set('db', databaseConnection) //this is an express function. Look more into it
   console.log('DB connected')
   app.listen(SERVER_PORT, () => console.log(`${SERVER_PORT} beers on the wall.`))
-  // dsadasda
+  
 })
 
--- src/Components/Wizard/Wizard.js
+++ src/Components/Wizard/Wizard.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-
+import axios from 'axios'
 
 
 class Wizard extends Component {
@@ -13,26 +13,58 @@ class Wizard extends Component {
         address: "",
         city: "",
         state: "",
-        zipcode:""
+        zipcode: 0
       };
     
   }
 
-// ******METHODS******//
+  // ****AXIOS SERVER CALLS**** //
+  // addHouse(){
+  //   const { name, address, city, state, zipcode } = this.state;
+  //   axios.post('/api/houses', {name, address, city, state, zipcode}).then(response => {
+  //     // const addData = response.data
+  //     this.setState({
+  //       name: addData,
+  //       address: addData,
+  //       city: addData,
+  //       state: addData,
+  //       zipcode: addData
+  //       // this.props.history.push('/')
+  //     })
+  //   })
+  // }
 
-  updateText( value ) {
-    this.setState({ name: value });
-    this.setState({ address: value });
-    this.setState({ city: value });
-    this.setState({ state: value });
-    this.setState({ zipcode: value });
-
+  addHouse() {
+    axios.post('/api/houses', this.state)
+      .then(res => {
+        this.props.history.push('/')
+      })
   }
   
 
 
+
+// ******METHODS******//
+
+  handleChangeName( value ) {
+    this.setState({ name: value });
+  }
+  handleChangeAddress( value ) {
+    this.setState({ address: value });
+  }
+  handleChangeCity( value ) {
+    this.setState({ city: value });
+  }
+  handleChangeState( value ) {
+    this.setState({ state: value });
+  }
+  handleChangeZipcode( value ) {
+    this.setState({ zipcode: value });
+  }
+  
+
   render() {
-    // const { name, address, city, state, zipcode } = this.state;
+    const { name, address, city, state, zipcode } = this.state;
 
     return (
       <div className = "wiz-contanier">
@@ -45,42 +77,45 @@ class Wizard extends Component {
           <h3>Property Name</h3>
           <input className="name__input"
             placeholder="Property Name?"
-            // value={ name }
-            onChange={ ( e ) => this.updateText( e.target.value ) } 
+            value={ name }
+            onChange={ ( e ) => this.handleChangeName( e.target.value ) } 
           />
+          
         </div>
         <div className="address">
           <h3>Address</h3>
           <input className="address_input"
             placeholder="Address..."
-            // value={ address }
-            onChange={ ( e ) => this.updateText( e.target.value ) } 
+            value={ address }
+            onChange={ ( e ) => this.handleChangeAddress( e.target.value ) } 
           />
         </div>
         <div className="city">
           <h3>City</h3>
           <input className="city_input"
             placeholder="City..."
-            // value={ city }
-            onChange={ ( e ) => this.updateText( e.target.value ) } 
+            value={ city }
+            onChange={ ( e ) => this.handleChangeCity( e.target.value ) } 
           />
         </div>
         <div className="state">
           <h3>State</h3>
           <input className="state_input"
             placeholder="State"
-            // value={ state }
-            onChange={ ( e ) => this.updateText( e.target.value ) } 
+            value={ state }
+            onChange={ ( e ) => this.handleChangeState( e.target.value ) } 
           />
         </div>
         <div className="zipcode">
           <h3>Zip</h3>
           <input className="zipcode_input"
             placeholder="Zipcode"
-            // value={ zipcode }
-            onChange={ ( e ) => this.updateText( e.target.value ) } 
+            value={ zipcode }
+            onChange={ ( e ) => this.handleChangeZipcode( e.target.value ) } 
           />
         </div>
+          <button className='btn' onClick={()=> this.addHouse()}>Complete</button>
+          {console.log(this.addHouse)}
 
       </div>
     );
